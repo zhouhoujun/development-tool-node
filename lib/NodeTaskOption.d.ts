705aae6070594c231f457c800d044d8ada1e1acf
zhouhoujun <houjunzhou@skspruce.net>
2016-11-03 11:59:11 +0800

    update

--- lib/NodeTaskOption.d.ts
+++ lib/NodeTaskOption.d.ts
@@ -1,7 +1,5 @@
 /// <reference types="mocha" />
 import { ITaskOption } from 'development-core';
 export interface INodeTaskOption extends ITaskOption {
-    tsconfig?: string;
-    tsBabelOption?: any;
     mochaOptions?: MochaSetupOptions;
 }
