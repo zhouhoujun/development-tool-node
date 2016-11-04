eb3654ab444611e27ea5188f962b84cb64384de6
zhouhoujun <houjunzhou@skspruce.net>
2016-11-04 18:12:24 +0800

    Comments doc in coding.

--- lib/NodeTaskOption.d.ts
+++ lib/NodeTaskOption.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="mocha" />
 import { ITaskOption } from 'development-core';
 export interface INodeTaskOption extends ITaskOption {
+    /**
+     * mocha test config.
+     *
+     * @type {MochaSetupOptions}
+     * @memberOf INodeTaskOption
+     */
     mochaOptions?: MochaSetupOptions;
 }
