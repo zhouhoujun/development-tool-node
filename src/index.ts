be5b2ef2633722a184501c96533f348a67361170
zhouhoujun <freebirds8281@hotmail.com>
2017-11-06 18:09:05 +0800

    update core, add some assets as  default config.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import * as _ from 'lodash';
-import { ITask, IAssertOption, IEnvOption, IContextDefine, ITaskContext, ITaskConfig, taskdefine } from 'development-core';
+import { ITask, IAssertOption, IEnvOption, IContextDefine, ITaskContext, ITaskConfig, taskdefine, Operation } from 'development-core';
 
 import { INodeTaskOption } from './NodeTaskOption';
 export * from './NodeTaskOption';
@@ -12,7 +12,18 @@ export class NodeContextDefine implements IContextDefine {
     loadConfig(option: IAssertOption, env: IEnvOption): ITaskConfig {
         // register default asserts.
         option.asserts = _.extend({
-            ts: { loader: 'development-assert-ts' }
+            css: Operation.default,
+            jpeg: Operation.default,
+            jpg: Operation.default,
+            gif: Operation.default,
+            png: Operation.default,
+            ioc: Operation.default,
+            svg: Operation.default,
+            ttf: Operation.default,
+            woff: Operation.default,
+            eot: Operation.default,
+            js: Operation.default | Operation.autoWatch,
+            ts: 'development-assert-ts'
         }, option.asserts || {});
 
 
